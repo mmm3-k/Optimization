08e475e9b36dc4860baec472da712a5c1f624b60
mmm3-k <64644159+mmm3-k@users.noreply.github.com>
2026-07-18 01:45:38 +0530

    Problem_V1_Casadi

---
+++ Problem_1.m
@@ -0,0 +1,28 @@
+clear all; clear; clc;
+import casadi.*;
+
+% Initial condition
+x0bar = [0; 0; 0];
+%Objective
+L = @(x) (x(1)^2 + x(2)^2 + x(3)^2);
+
+g = x(1) + x(2) + x(3);
+
+decision_variable = x;
+
+%Create NLP solver
+nlp = struct('x',x,'f',L,,'g',g);
+opts = struct;
+opts.ipopt.max_iter = 1000;
+opts.ipopt.print_level = 0;
+opts.ipopt.acceptable_tol = 1e-8;
+
+solver = nlpsol('solver','ipopt',nlp,opts);
+sol = solver('x0',x0bar,'lbx', -inf, 'ubx', inf, 'lbg', 10,'ubg', 10);
+
+%Optimal X values are
+x_sol = full(sol.x);
+
+%Optimal Objective value is 
+min_value = full(sol.f);
+
